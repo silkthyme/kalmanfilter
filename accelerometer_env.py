ad2b506264134b64e48a262f07e37af195d9b859
christina <christinahuangji@gmail.com>
2020-02-21 16:27:26 -0800

    fix acceleration typos

--- accelerometer_env.py
+++ accelerometer_env.py
@@ -1,6 +1,6 @@
 # Accelerometer Intergration
-# Objective: Position detection with velocity derived through accelaration
-# Key Components: 1) Model accelaration as measured by the Accelarometer
-#                 2) Integrate Accelaration to determine velocity
+# Objective: Position detection with velocity derived through acceleration
+# Key Components: 1) Model acceleration as measured by the Accelarometer
+#                 2) Integrate Acceleration to determine velocity
 # Assumptions: - Sample Measured position with 0.01km/hr^2 standard deviation
 
